f21601dd4bfb1878fbc2bc788cf303f39af12474
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-05-17 14:53:03 +0530

    Update rip_owner_msg.py

--- tobrot/helper_funcs/rip_owner_msg.py
+++ tobrot/helper_funcs/rip_owner_msg.py
@@ -1,11 +1,14 @@
 import asyncio 
 
-async def rip_owner_msg(message):
+#async def rip_owner_msg(message):
 
-  if message is not None:
+#  if message is not None:
 
-    if message.text.lower().startswith("@The_Fourth_Minato"):
+ #   if message.text.lower().startswith("@The_Fourth_Minato"):
 
-      await message.reply_text("the owner of this bot @The_Fourth_Minato is ded 🥲")
+ #     await message.reply_text("the owner of this bot @The_Fourth_Minato is ded 🥲")
 
-      return 
+ #     return 
+
+if message.text.lower().startswith("@The_Fourth_Minato"):
+  print("the owner of this bot is ded")
