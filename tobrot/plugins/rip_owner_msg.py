48309ed8a9320bc03f5a42dda57b192c9e128f7a
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-05-17 14:38:51 +0530

    Update rip_owner_msg.py

--- tobrot/plugins/rip_owner_msg.py
+++ tobrot/plugins/rip_owner_msg.py
@@ -2,7 +2,7 @@ import asyncio
 
 async def rip_owner_msg(message):
   if message in not None:
-    if "@The_Fourth_Minato" is in message:
+    if message.text.lower().startswith("@The_Fourth_Minato"):
       await message.reply_text("the owner of this bot @The_Fourth_Minato is ded 🥲")
       return 
  
